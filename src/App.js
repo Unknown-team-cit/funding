cf6147c37642d825ed27c039b854a5397a5b0572
naveen.kumar <naveen.kumar@gogosoon.com>
2021-03-13 13:25:13 +0530

    Adds navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { Grid } from '@material-ui/core';
-import './App.css';
-import Cards from './components/Card';
-import Register from './components/Register.jsx';
+import { Grid } from "@material-ui/core";
+import "./App.css";
+// import Cards from "./components/Card";
+import Header from "./components/Header";
+// import Login from "./components/Login";
+import Register from "./components/Register.jsx";
 
 function App() {
   return (
     <div>
-      <Grid direction='row'  container justify='center'>
-
-      {/* <Cards heading='naveen' body='this body will have all the required contents' /> */}
-      <Register/>
+      <Header reg={true} />
+      <Grid direction="row" container justify="center">
+        {/* <Cards heading='naveen' body='this body will have all the required contents' /> */}
+        <Register />
+        {/* <Login /> */}
       </Grid>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appbar: {
+    backgroundColor: "#311b92",
+  },
+  button: {
+    textTransform: "none",
+    fontFamily: "Poppins, sans-serif",
+    fontSize: "16px",
+  },
+}));
+
+function Header(props) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appbar} position="static">
+        <Toolbar>
+          <img
+            src="https://www.collegeinnovators.com/wp-content/uploads/2018/10/startup-png-22-300x223.png"
+            alt="no img"
+            width="80px"
+            height="64px"
+            id="image"
+          />
+          <Typography variant="h6" className={classes.title}></Typography>
+          {!props.reg && (
+            <Button className={classes.button} color="inherit">
+              Register
+            </Button>
+          )}
+          <Button className={classes.button} color="inherit">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;
